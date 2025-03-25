29af2154f58d2de58be223222588a648d9af5f3a
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-25 10:54:09 +0100

    Create skill-analysis.component.ts

---
+++ src/app/features/analysis/skill-analysis/skill-analysis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-analysis',
+  standalone: true,
+  imports: [],
+  templateUrl: './skill-analysis.component.html',
+  styleUrl: './skill-analysis.component.scss'
+})
+export class SkillAnalysisComponent {
+
+}

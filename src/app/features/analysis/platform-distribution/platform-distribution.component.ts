b0564730895edf30182e9c2b3b39316a374ef998
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-25 08:00:03 +0100

    Create platform-distribution.component.ts

---
+++ src/app/features/analysis/platform-distribution/platform-distribution.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-platform-distribution',
+  standalone: true,
+  imports: [],
+  templateUrl: './platform-distribution.component.html',
+  styleUrl: './platform-distribution.component.scss'
+})
+export class PlatformDistributionComponent {
+
+}

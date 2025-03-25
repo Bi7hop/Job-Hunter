7c6136388e2507241ec43a991a1de64893d6e039
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-25 08:00:20 +0100

    Create salary-comparison.component.ts

---
+++ src/app/features/analysis/salary-comparison/salary-comparison.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salary-comparison',
+  standalone: true,
+  imports: [],
+  templateUrl: './salary-comparison.component.html',
+  styleUrl: './salary-comparison.component.scss'
+})
+export class SalaryComparisonComponent {
+
+}

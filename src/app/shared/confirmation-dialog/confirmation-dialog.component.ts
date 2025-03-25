0721a68582c4466744ac3b345cd5082ba33cecaf
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-25 07:14:04 +0100

    Create confirmation-dialog.component.ts

---
+++ src/app/shared/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,40 @@
+import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+export interface ConfirmationDialogData {
+  title: string;
+  message: string;
+  confirmText: string;
+  cancelText: string;
+  icon?: string;
+}
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss']
+})
+export class ConfirmationDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogData
+  ) {}
+
+  cancel(): void {
+    this.dialogRef.close(false);
+  }
+
+  confirm(): void {
+    this.dialogRef.close(true);
+  }
+}

0a263c1a1e96bef8571c9c782b382e53c5bd1a40
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-26 07:18:45 +0100

    Create portal-selector.component.ts

---
+++ src/app/features/job-tracker/portal-selector/portal-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portal-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './portal-selector.component.html',
+  styleUrl: './portal-selector.component.scss'
+})
+export class PortalSelectorComponent {
+
+}

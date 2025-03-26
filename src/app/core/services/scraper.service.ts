a1cc5107d08206bbf99c5326e091f1fc62805b3b
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-26 11:59:59 +0100

    Update scraper.service.ts

--- src/app/core/services/scraper.service.ts
+++ src/app/core/services/scraper.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScraperService {
+  private apiUrl = 'http://localhost:5000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  triggerScrape(platforms: string[], keywords?: string, location?: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/jobs/scrape`, {
+      platforms,
+      keywords,
+      location
+    });
+  }
 }

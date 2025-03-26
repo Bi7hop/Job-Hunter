65f49fe4d4f78ce54a790e043266f428ac47fc01
Marcel Menke <marcel.menke1981@gmail.com>
2025-03-26 07:19:11 +0100

    Create search-criteria.component.ts

---
+++ src/app/features/job-tracker/search-criteria/search-criteria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-criteria',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-criteria.component.html',
+  styleUrl: './search-criteria.component.scss'
+})
+export class SearchCriteriaComponent {
+
+}
